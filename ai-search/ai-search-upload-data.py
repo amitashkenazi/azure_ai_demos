8305433588904e5bc0ca48632f6b7a650a6d3d43
Your Name <amit>
2023-12-20 17:52:58 +0200

    first commit - ai-search-demo

---
+++ ai-search/ai-search-upload-data.py
@@ -0,0 +1,63 @@
+import requests
+import json
+from cosmosdb_handler import store, get, delete_item, delete_all_matches, update_item
+from pprint import pprint
+from openai.embeddings_utils import get_embedding, cosine_similarity
+import openai
+import os
+
+openai.api_type = "azure"
+openai.api_key = os.environ['OPENAI_API_KEY']
+openai.api_base = os.environ['OPENAI_ENDPOINT']
+openai.api_version = "2022-12-01"
+print(os.environ['OPENAI_ENDPOINT'])
+
+
+service_name = 'aisearch-aash'
+index_name = 'faqdemo'
+api_version = '2020-06-30'
+endpoint = f'https://{service_name}.search.windows.net/indexes/{index_name}/docs/index?api-version={api_version}'
+
+headers = {
+    'Content-Type': 'application/json',
+    'api-key': os.environ['AI_SEARCH_KEY']
+}
+
+if __name__ == "__main__":
+    chunks = []
+
+    docs = get()
+    for d in docs:
+        print(d["id"])
+        print(d["question"])
+        print(d["answer"])
+        embedding_q = get_embedding(
+            d["question"],
+            engine="text-embedding-ada-002"
+        )
+        print("embedding_q")
+        embedding_a = get_embedding(
+            d["answer"],
+            engine="text-embedding-ada-002"
+        )
+
+        chunk = {
+            "id": d["id"],
+            "question": d["question"],
+            "answer": d["answer"],
+            "QuestionVector": embedding_q,
+            "AnswerVector": embedding_a,
+            "Tags": ["FAQ", "Azure"]
+        }
+        chunks.append(chunk)
+
+    batch = {
+        "value": chunks
+    }
+
+    # Convert the batch to a JSON string
+    payload = json.dumps(batch)
+    print("sending...")
+    response = requests.post(endpoint, headers=headers, data=payload)
+    print(response.status_code)
+    print(response.text)
---
+++ ai-search/create-index.py
@@ -0,0 +1,148 @@
+import requests
+import json
+import os
+
+url = "https://aisearch-aash.search.windows.net/indexes/faqdemo?api-version=2023-11-01"
+
+payload = json.dumps({
+  "name": "faqdemo",
+  "fields": [
+    {
+      "name": "id",
+      "type": "Edm.String",
+      "searchable": False,
+      "filterable": True,
+      "retrievable": True,
+      "sortable": False,
+      "facetable": False,
+      "key": True,
+      "indexAnalyzer": None,
+      "searchAnalyzer": None,
+      "analyzer": None,
+      "fields": []
+    },
+    {
+      "name": "question",
+      "type": "Edm.String",
+      "searchable": True,
+      "filterable": True,
+      "retrievable": True,
+      "sortable": True,
+      "facetable": False,
+      "key": False,
+      "indexAnalyzer": None,
+      "searchAnalyzer": None,
+      "analyzer": None,
+      "fields": []
+    },
+    {
+      "name": "QuestionVector",
+      "type": "Collection(Edm.Single)",
+      "searchable": True,
+      "retrievable": True,
+      "dimensions": 1536,
+      "vectorSearchProfile": "my-vector-profile"
+    },
+    {
+      "name": "answer",
+      "type": "Edm.String",
+      "searchable": True,
+      "filterable": True,
+      "retrievable": True,
+      "sortable": True,
+      "facetable": False,
+      "key": False,
+      "indexAnalyzer": None,
+      "searchAnalyzer": None,
+      "analyzer": "en.microsoft",
+      "fields": []
+    },
+    {
+      "name": "AnswerVector",
+      "type": "Collection(Edm.Single)",
+      "searchable": True,
+      "retrievable": True,
+      "dimensions": 1536,
+      "vectorSearchProfile": "my-vector-profile"
+    },
+    {
+      "name": "Tags",
+      "type": "Collection(Edm.String)",
+      "searchable": True,
+      "filterable": True,
+      "retrievable": True,
+      "sortable": False,
+      "facetable": True,
+      "key": False,
+      "indexAnalyzer": None,
+      "searchAnalyzer": None,
+      "analyzer": None,
+      "fields": []
+    }
+],
+  "corsOptions": {
+    "allowedOrigins": [
+      "*"
+    ],
+    "maxAgeInSeconds": 60
+  },
+  "vectorSearch": {
+    "algorithms": [
+      {
+        "name": "my-hnsw-config",
+        "kind": "hnsw",
+        "hnswParameters": {
+          "m": 4,
+          "efConstruction": 400,
+          "efSearch": 500,
+          "metric": "cosine"
+        }
+      },
+      {
+        "name": "my-eknn-config",
+        "kind": "exhaustiveKnn",
+        "exhaustiveKnnParameters": {
+          "metric": "cosine"
+        }
+      }
+    ],
+    "profiles": [
+      {
+        "name": "my-vector-profile",
+        "algorithm": "my-hnsw-config"
+      }
+    ]
+  },
+  "semantic": {
+    "configurations": [
+      {
+        "name": "my-semantic-config",
+        "prioritizedFields": {
+        #   "titleField": {
+        #     "fieldName": "HotelName"
+        #   },
+            
+          "prioritizedContentFields": [
+            {
+              "fieldName": "answer"
+            }
+          ],
+          "prioritizedKeywordsFields": [
+            {
+              "fieldName": "Tags"
+            }
+          ]
+        }
+      }
+    ]
+  }
+})
+headers = {
+  'Content-Type': 'application/json',
+  'api-key': os.environ['AI_SEARCH_KEY']
+}
+
+response = requests.request("PUT", url, headers=headers, data=payload)
+
+print(response.status_code)
+print(response.text)
